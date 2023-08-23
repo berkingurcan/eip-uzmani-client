67b5836d83c94cc0aafe90d0bbe14347efc714f6
berkingurcan <berkingurcan@yandex.com>
2023-08-23 09:11:28 +0300

    feat add retrieve via langchain and promt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { kv } from '@vercel/kv'
 import { NextRequest } from 'next/server'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { OpenAIStream, StreamingTextResponse, VercelChatMessage } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
 import { ChatOpenAI } from 'langchain/chat_models/openai'
@@ -16,12 +16,35 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`
+}
+
+const TEMPLATE = `You are a Senior Blockchain Developer who has great knowledge about Ethereum Improvement Proposals. 
+You are helping people about Ethereum, Solidity, EIP's etc. If you don't have the asked information, just say the truth.
+ 
+Current conversation:
+{chat_history}
+ 
+User: {input}
+AI:`
+
 const openai = new OpenAIApi(configuration)
 
 export async function POST(req: Request) {
   const json = await req.json()
   const { messages, previewToken } = json
+  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage)
+  const currentMessageContent = messages[messages.length - 1].content
   const userId = (await auth())?.user.id
+
+  const prompt = PromptTemplate.fromTemplate(TEMPLATE)
+
+  const model = new ChatOpenAI({
+    temperature: 0.8
+  })
+
+  const outputParser = new BytesOutputParser()
 
   if (!userId) {
     return new Response('Unauthorized', {
@@ -40,9 +63,14 @@ export async function POST(req: Request) {
     stream: true
   })
 
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
+  const chain = prompt.pipe(model).pipe(outputParser)
+ 
+  const stream = await chain.stream({
+    chat_history: formattedPreviousMessages.join('\n'),
+    input: currentMessageContent
+  })
+
+  const title = json.messages[0].content.substring(0, 100)
       const id = json.id ?? nanoid()
       const createdAt = Date.now()
       const path = `/chat/${id}`
@@ -55,7 +83,7 @@ export async function POST(req: Request) {
         messages: [
           ...messages,
           {
-            content: completion,
+            content: currentMessageContent,
             role: 'assistant'
           }
         ]
@@ -65,8 +93,6 @@ export async function POST(req: Request) {
         score: createdAt,
         member: `chat:${id}`
       })
-    }
-  })
-
+ 
   return new StreamingTextResponse(stream)
 }
